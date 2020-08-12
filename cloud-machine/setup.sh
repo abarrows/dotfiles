62b437a6904a39e03a79dcb919cf041d0e19ab11
Andrew Barrows <abarrows@amuniversal.com>
2020-08-12 15:41:15 -0500

    shellchecked the setup script.

--- cloud-machine/setup.sh
+++ cloud-machine/setup.sh
@@ -19,6 +19,6 @@
 # ## setup and install oh-my-zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # cp -R /root/.oh-my-zsh /home/$USERNAME
-# cp /root/.zshrc /home/$USERNAME
+# sudo cp /root/.zshrc /home/$USERNAME
 # sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 # chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
