6486da0cc3cb4a7b6f1e12c802132cb10fc2f20a
Andrew Barrows <acbarrows@gmail.com>
2020-09-16 12:34:43 -0500

    BIN FILE: Adding solargraph/nokogiri fix

---
+++ acb_bin/fix-solargraph-nokogiri.sh
@@ -0,0 +1,9 @@
+# When facing the following error:
+# /Users/abarrows/.rvm/gems/ruby-2.5.3@dilbert/gems/nokogiri-1.10.9/lib/nokogiri.rb:32:in
+# `require': incompatible library version -
+# /Users/abarrows/.rvm/gems/ruby-2.5.3@dilbert/gems/nokogiri-1.10.9/lib/nokogiri/nokogiri.bundle
+# (LoadError)
+
+# SOLUTION: Solargraph requires an older version of nokogiri (1.10.09 or older)
+gem uninstall nokogiri -v 1.10.10
+gem uninstall nokogiri -v 1.10.09
