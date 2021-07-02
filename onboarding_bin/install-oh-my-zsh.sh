4bcf1223a723e05600570b5ce87c6dd349b49d6a
Andrew Barrows <acbarrows@gmail.com>
2021-07-02 16:11:53 -0500

    Making theme on one line.

--- onboarding_bin/install-oh-my-zsh.sh
+++ onboarding_bin/install-oh-my-zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -f "$ZSH/oh-my-zsh.sh" ]]; then
-  echo "Oh my Zsh IS installed and is using this theme: " && echo $ZSH_THEME
+  echo "Oh my Zsh IS installed and is using this theme: \$ZSH_THEME"
 else
   echo "Oh my Zsh is not installed!  Installing now..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
