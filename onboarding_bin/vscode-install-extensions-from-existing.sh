214a1ea453db2d4a58e080bc777b38f12afea25e
Andrew Barrows <acbarrows@gmail.com>
2021-08-09 18:03:34 -0500

    1. Updating the schema.org in hopes that it will fix the bitbucket reference. 2. Adding more definitions to the dictionary 3. Updating the pathing to not be in the root .vscode anymore.

--- onboarding_bin/vscode-install-extensions-from-existing.sh
+++ onboarding_bin/vscode-install-extensions-from-existing.sh
@@ -2,11 +2,11 @@
 
 # Use IDE_PATH if you are getting your ide dynamically.
 echo "1. VSCode - Retrieving all extensions..."
-code --list-extensions > ../.vscode/extensions.txt
+code --list-extensions > ../engineering/ide/.vscode/extensions.txt
 
 echo "2. VSCode - Parsing all extensions..."
 while read -r my_extension; do
-  code --install-extension $my_extension
-done <.vscode/extensions.txt
+  code --install-extension "$my_extension"
+done <engineering/ide/.vscode/extensions.txt
 
 echo "\n\n\n3. VSCode - All done!"
--- onboarding_bin/vscode-jsonify-extensions.sh
+++ onboarding_bin/vscode-jsonify-extensions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "1. VSCode - Retrieving all extensions..."
-code --list-extensions > .vscode/extensions.json
+code --list-extensions > engineering/ide/.vscode/extensions.json
 
 echo "2. VSCode - Parsing all extensions into comma separated list..."
-awk '{ printf "\"%s\",\n", $0 }' .vscode/extensions.txt > .vscode/extensions.json
+awk '{ printf "\"%s\",\n", $0 }' engineering/ide/.vscode/extensions.txt > engineering/ide/.vscode/extensions.json
 
 echo "\n\n\n3. VSCode - All done!"
