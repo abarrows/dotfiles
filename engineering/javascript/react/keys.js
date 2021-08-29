f48bcccedab9d538ab1375549c53160eccef5aad
Andrew Barrows <acbarrows@gmail.com>
2021-08-29 10:35:50 -0500

    Adding javascript dotenv for package.json (keys.js) which map to dotenv.

---
+++ engineering/javascript/react/keys.js
@@ -0,0 +1,9 @@
+// keys.js ======
+module.exports = {
+  APPLICATION_NAME: process.env.APPLICATION_NAME,
+  DEPLOY_ENV: process.env.DEPLOY_ENV,
+  APP_NAME: process.env.APP_NAME,
+  APP_PORT: process.env.APP_PORT,
+  APP_VERSION: process.env.APP_VERSION,
+  STORYBOOK_PORT: process.env.STORYBOOK_PORT
+};
