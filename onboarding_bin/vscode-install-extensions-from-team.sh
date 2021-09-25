98f1bb1ab19e0bb9575367c41563ed312465d7a8
Andrew Barrows <acbarrows@gmail.com>
2021-09-25 11:08:20 -0500

    Making the vs code extensions script more understandable.

--- onboarding_bin/vscode-install-extensions-from-team.sh
+++ onboarding_bin/vscode-install-extensions-from-team.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script will NOT use the version controlled list of extensions from the
+# team and assumes you have your own list of extensions installed.  NOTE: This
+# is typically only used by the maintainer or in circumstances where an engineer
+# wants to record their own VS code extensions.
+
 # Use IDE_PATH if you are getting your ide dynamically.
 echo "1. VSCode - Retrieving all extensions..."
 code --list-extensions > ../engineering/ide/.vscode/extensions.txt
