edf73256ef055c5bcd87e17a8daf84dbd0871872
Andrew Barrows <acbarrows@gmail.com>
2022-02-11 17:13:33 -0600

    Adding an install to RVM onboarding script.

---
+++ onboarding_bin/install-rvm.sh
@@ -0,0 +1,10 @@
+# RVM is the best way to install Ruby.  It allows for multiple versions of Ruby
+# on your machine.  It also allows for multiple versions of RubyGems
+
+# Step 1: Requires homebrew package: gnupg (Should be installed with Brewfile.ruby)
+# Step 2: Install GPG key for rvm
+# gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys
+# 409B6B1796C275462A1703113804BB82D39DC0E3
+# 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# Step 3: Install RVM
+# \curl -sSL https://get.rvm.io | bash
