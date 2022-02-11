0b18ba4f8bfbf88db15428e1ebe4211033acacaf
Andrew Barrows <acbarrows@gmail.com>
2022-02-11 17:13:51 -0600

    Adding a install extensions for RoR.

---
+++ onboarding_bin/vscode-install-extensions-ruby.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will NOT use the version controlled list of extensions from the
+# team and assumes you have your own list of extensions installed.  NOTE: This
+# is typically only used by the maintainer or in circumstances where an engineer
+# wants to record their own VS code extensions.
+
+# Use IDE_PATH if you are getting your ide dynamically.
+# echo "1. VSCode - Retrieving all extensions..."
+# code --list-extensions >../engineering/ide/.vscode/extensions.txt
+
+echo "2. VSCode - Parsing all extensions..."
+while read -r my_extension; do
+  code --install-extension "$my_extension"
+done <engineering/ide/.vscode/extensions-ruby.txt
+
+echo "\n\n\n3. VSCode - Ruby extensions are all done!"
