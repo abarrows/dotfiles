4a44892cb7672250adb46dd3565c5716760483fa
Andrew Barrows <abarrows@amuniversal.com>
2022-05-19 16:20:24 -0500

    My Signed Commit.
    Signed-off-by: Andrew Barrows <abarrows@amuniversal.com>

--- onboarding_bin/install-homebrew.sh
+++ onboarding_bin/install-homebrew.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # First check if homebrew is installed and if not
-if [ ! -x /usr/local/bin/brew ]; then
+if [ ! -x /usr/local/bin/brew ] | [ ! -x /opt/homebrew ]; then
   echo "Homebrew is NOT already installed."
-  arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew IS already installed."
 fi
