a4fcc1c30d7b7cd77b984080325e58d75b4c0666
Andrew Barrows <abarrows@amuniversal.com>
2022-05-19 16:24:28 -0500

    My Signed Commit.
    Signed-off-by: Andrew Barrows <abarrows@amuniversal.com>

--- shell/.theme.zsh
+++ shell/.theme.zsh
@@ -6,12 +6,12 @@
 
 # Starship Theme
 # Instructions: https://guinuxbr.com/en/posts/zsh+oh-my-zsh+starship/
-if [[ -f "/usr/local/bin/starship" ]]; then
+if [[ -f "/opt/homebrew/bin/starship" ]]; then
   echo 'Theme should be sourced: Starship.'
   eval "$(starship init zsh)"
 else
   # NOTE: Starship is brought in via Homebrew.base, below is a command line install.
-  # echo 'Cloning the theme: Starship'
+  echo 'Starship ZSH theme SHOULD have been installed with base profile onboarding.  Please check Brewfile.base'
   # sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 fi
 
