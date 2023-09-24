da871d15b7f2c2777085c60b44ec5c18a8a1f14b
Andrew Barrows <acbarrows@gmail.com>
2023-09-24 17:19:47 -0500

    Fixing the shell formatter.

--- onboarding_bin/vscode-install-extensions-from-local.sh
+++ onboarding_bin/vscode-install-extensions-from-local.sh
@@ -7,11 +7,11 @@
 
 # Use IDE_PATH if you are getting your ide dynamically.
 echo "1. VSCode - Retrieving all extensions..."
-code --list-extensions > ../engineering/ide/.vscode/extensions_base.txt
+echo "code --list-extensions >../engineering/ide/.vscode/extensions_base.txt"
 
 echo "2. VSCode - Parsing all extensions..."
 while read -r my_extension; do
   code --install-extension "$my_extension"
 done <engineering/ide/.vscode/extensions.txt
 
-echo "\n\n\n3. VSCode - All done!"
+echo -e "\n\n\n3. VSCode - All done!"
