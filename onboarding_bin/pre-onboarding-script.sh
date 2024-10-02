170d911bb86c85e128671ab41af1e475d3269913
andyb <andyb@retailsuccess.com>
2024-10-02 13:25:19 -0500

    Refactor shell aliases for digital products

--- onboarding_bin/pre-onboarding-script.sh
+++ onboarding_bin/pre-onboarding-script.sh
@@ -1,7 +1,7 @@
 cd ~/Documents &&
   mkdir -p AMU/repos/amu-development-team &&
   cd ~/Documents/AMU/repos/amu-development-team &&
-  git clone https://${YOUR_GITHUB_PAT}:x-oauth-basic@github.com/Andrews-McMeel-Universal/amu-onboarding.git &&
+  git clone https://"${YOUR_GITHUB_PAT}":x-oauth-basic@github.com/${COMPANY_NAME}/amu-onboarding.git &&
   cd amu-onboarding &&
   cp .envrc.example .envrc &&
   open .envrc
